a3e4bb6c070c9fde313d67c50acfb846932602d0
angelmaria <angeld05@live.com>
2019-09-04 11:47:42 -0400

    Modelos de datos (MVC)

---
+++ src/app/Modules/Juego.ts
@@ -0,0 +1,7 @@
+export class Juego {
+  constructor(
+    public nombre: string,
+    public precio: number,
+    public creador: string
+  ) {}
+}
--- src/app/Videojuego/videojuego.component.ts
+++ src/app/Videojuego/videojuego.component.ts
@@ -1,5 +1,6 @@
 // Primero importamos el componente
 import {Component, OnInit, OnDestroy, DoCheck} from '@angular/core';
+import {Juego} from '../Modules/Juego';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -7,25 +8,33 @@ import {Component, OnInit, OnDestroy, DoCheck} from '@angular/core';
   templateUrl: 'videojuego.component.html'
 })
 export class VideojuegoComponent implements OnInit, OnDestroy, DoCheck {
-  private titulo:string;
-  private listado:string;
+  private titulo: string;
+  private listado: string;
+  private juegos: Array<Juego>;
+
   constructor() {
     this.titulo = 'Componente de Videojuego (dinamico)';
     this.listado = 'Listado de los mas populares (dinamico)';
-    console.log('Se cargo mi primero componente');
+    // console.log('Se cargo mi primero componente');
+
+    //  Arreglo con objetos dentro
+    this.juegos = [
+      new Juego('GTA', 100, 'RochStar'),
+      new Juego('SSMB', 80, 'Nintendo'),
+      new Juego('Samus', 5, 'Nintendo'),
+    ];
+    console.log(this.juegos);
   }
   // Hooks, evento para cuando se inicia el componente (despues del constructor)
   ngOnInit(): void {
-    console.log('Soy el OnInit del compoente');
+    // console.log('Soy el OnInit del compoente');
   }
   // Hooks, evento para cuando cambia algo dentro de componente
   ngDoCheck(): void {
-    console.log('Soy DoCheck del componente, estoy donde encuentre cambios XDXD');
+    // console.log('Soy DoCheck del componente, estoy donde encuentre cambios XDXD');
   }
   // Hooks, evento para cuando se destruye un componente
   ngOnDestroy(): void {
-    console.log('Soy OnDestroy del componente');
+    // console.log('Soy OnDestroy del componente');
   }
-
-
 }
