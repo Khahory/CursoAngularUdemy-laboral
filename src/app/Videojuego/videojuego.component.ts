9373b489b5f4d4fc223659e1ae14ecd938b25bc2
angelmaria <angeld05@live.com>
2019-09-04 09:18:55 -0400

    Primer componente (dinamico)

--- src/app/Videojuego/videojuego.component.ts
+++ src/app/Videojuego/videojuego.component.ts
@@ -4,17 +4,14 @@ import {Component} from '@angular/core';
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'videojuego',
-  template: `
-  <h2>Componente de Videojuego</h2>
-    <ul>
-        <li>GTA</li>
-        <li>Mario</li>
-        <li>Kirby</li>
-    </ul>
-  `
+  templateUrl: 'videojuego.component.html'
 })
 export class VideojuegoComponent {
+  private titulo:string;
+  private listado:string;
   constructor() {
+    this.titulo = 'Componente de Videojuego (dinamico)';
+    this.listado = 'Listado de los mas populares (dinamico)';
     console.log('Se cargo mi primero componente');
   }
 }
